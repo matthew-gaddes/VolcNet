8e0a2bf260845f4d1a4f79ce6d29894e5a83a1e7
matthew.gaddes <gaddes.matthew@gmail.com>
2022-10-03 11:02:30 +0100

    Fix type in annotation_backup. Add function to label all the possible interferograms in a list of volcnet files.

--- volcnet/labelling.py
+++ volcnet/labelling.py
@@ -12,8 +12,77 @@ import pdb
 
 #%%
 
-def volcnet_labeller(ifg_name, persistent_defs, transient_defs, noise_threshold = 0.02):
-    """ Given VolcNet lables (persistent and transient defs) for a time series, and the acquisiiton dates for an inteferogram,
+
+def label_volcnet_files(volcnet_files, def_min = 0.05):
+    """ Given a list of volcnet files, create all possible labels for the interferograms that can be made between all possible acquisitions.  
+    Note that this doesn't actually make the interferograms, so the outputs are small (i.e. not GBs)
+    
+    Inputs:
+        volcnet_files | list of strings | list of volcnet files to label.  
+        def_min | float | magnitude of deformation must be larger than this to be classed as deformation.  Units: metres.  
+        
+    Returns:
+        labels_dyke | many x 4 | Interferograms that are labelled as dyke.  First coumn is file number, second is acquisition 1, third is acquisition 2, fourth is deformation magnitude
+        labels_sill | many x 4 |
+        labels_atmo | many x 4 |
+
+    History:
+        2022_10_03 | MEG | Written
+    """
+    import numpy as np
+    import pickle
+        
+    labels_dyke = np.nan * np.zeros((1, 4))                                         # initialise, 1 x 4.  First coumn is file number, second is acquisition 1, third is acquisition 2, fourth is deformation magnitude
+    labels_sill = np.nan * np.zeros((1, 4))                                          # as above, for next label type
+    labels_atmo = np.nan * np.zeros((1, 4))                                          # as above, for next label type
+    
+    for file_n, volcnet_file in enumerate(volcnet_files):                               # loop through files.  
+        
+        print(f"Opening file: {volcnet_file.split('/')[-1]}")
+        # 1: Open the file
+        with open(volcnet_file, 'rb') as f:
+            displacement_r3 = pickle.load(f)
+            tbaseline_info = pickle.load(f)
+            persistent_defs = pickle.load(f)
+            transient_defs = pickle.load(f)
+        print(f"The interferograms are of size: {displacement_r3['mask'].shape}")
+        #n_acq, ny, nx = displacement_r3['cumulative'].shape
+       
+        for acq_n1, acq_1 in enumerate(tbaseline_info['acq_dates']):                                                # 
+            for acq_n2, acq_2 in enumerate(tbaseline_info['acq_dates']):
+                if acq_1 == acq_2:                                                                                                          # will just be zeros so ignore.   
+                    pass                                                                                                                      # just leave as nans          
+                else:
+                    #ifg = displacement_r3['cumulative'][acq_n2,] - displacement_r3['cumulative'][acq_n1,]                                    # make the ifg between the two acquisitions.  
+                    def_predicted, sources, def_location = label_volcnet_ifg(f"{acq_1}_{acq_2}", persistent_defs, transient_defs)              # label the ifg, def_location is still in terms of lon and lat
+                    if (np.abs(def_predicted) < def_min):                                                                                    # if the deformation is less than the threshold selected       
+                        # def_magnitudes[acq_n1, acq_n2] = 0.                                                                                  # set deformation to 0.           
+                        # labels[acq_n1, acq_n2] = 2                                                                                           # and label as no def
+                        labels_atmo[-1] = np.array([file_n, acq_n1, acq_n2, def_predicted])
+                        labels_atmo = np.vstack((labels_atmo, np.nan * np.zeros((1, 4))))
+                    else:                                                                                                                    # else deformation is big enough
+         #               def_magnitudes[acq_n1, acq_n2] = def_predicted                                                                       # record deformation
+                        if sources[0] == 'dyke':
+                            # labels[acq_n1, acq_n2] = 0                                                                                      # label as dyke                    
+                            labels_dyke[-1] = np.array([file_n, acq_n1, acq_n2, def_predicted])
+                            labels_dyke = np.vstack((labels_dyke, np.nan * np.zeros((1, 4))))
+                        elif sources[0] == 'sill':
+    #                        labels[acq_n1, acq_n2] = 1                                                                                      # label as sill
+                            labels_sill[-1] = np.array([file_n, acq_n1, acq_n2, def_predicted])
+                            labels_sill = np.vstack((labels_sill, np.nan * np.zeros((1, 4))))
+        #data_indexs[volcnet_file.split('/')[-1]] = (def_magnitudes, labels)
+    labels_dyke = labels_dyke[:-1,:]
+    labels_sill = labels_sill[:-1,:]
+    labels_atmo = labels_atmo[:-1,:]
+    return labels_dyke, labels_sill, labels_atmo
+
+
+
+
+#%%
+
+def label_volcnet_ifg(ifg_name, persistent_defs, transient_defs, noise_threshold = 0.02):
+    """ Given VolcNet labels (persistent and transient defs) for a time series, and the acquisiiton dates for an inteferogram,
     Create a label for it, calculate the amount of deformaiton expected, and the bounding box.  
     
     Inputs:
